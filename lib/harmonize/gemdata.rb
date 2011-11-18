b0b629017fac804d0c50406abdb2644ab142546c
Bram Swenson <bram.swenson@akqa.com>
2011-11-18 12:05:10 -0800

    [Version] bump

--- lib/harmonize/gemdata.rb
+++ lib/harmonize/gemdata.rb
@@ -1,7 +1,7 @@
 module Harmonize
   module Gemdata
     Name    = 'harmonize'
-    Version = '0.0.4'
+    Version = '0.0.5'
     Authors = [ 'Bram Swenson' ]
     Email   = [ 'bram@craniumisajar.com' ]
     Summary = <<-END_SUMMARY
